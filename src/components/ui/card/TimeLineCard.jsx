e0ac354a0c8de547f40ba72cbd7a9739be6bc4da
Anawar Hossain <anwarhossainhossain01@gmail.com>
2026-04-15 12:25:24 +0600

    Add TimeLineCard component and refactor TimeLine to use it for rendering call details

---
+++ src/components/ui/card/TimeLineCard.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+const TimeLineCard = ({ call }) => {
+    return (
+      <div>
+        <div className="border-b bg-white shadow rounded-lg mt-5">
+          <div className="flex items-center gap-3 py-4 px-2.5">
+            <span className="font-bold text-2xl">{call.icon}</span>
+            <div className="">
+              <div className="flex items-center gap-2">
+                <h1 className="font-bold text-lg">{call.type}</h1>
+                <p className="text-gray-500">
+                  <span>with</span> {call.expecteFriend.name}
+                </p>
+              </div>
+
+              <h1 className="text-gray-500">
+                {new Date(call.timestamp).toLocaleDateString("en-US", {
+                  year: "numeric",
+                  month: "long",
+                  day: "numeric",
+                })}
+              </h1>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+};
+
+export default TimeLineCard;
--- src/pages/timeLine/TimeLine.jsx
+++ src/pages/timeLine/TimeLine.jsx
@@ -1,5 +1,6 @@
 import React, { useContext } from "react";
 import { Context } from "../../context/Context";
+import TimeLineCard from "../../components/ui/card/TimeLineCard";
 
 const TimeLine = () => {
   const { callButtons } = useContext(Context);
@@ -22,27 +23,7 @@ const TimeLine = () => {
           <div>
             {callButtons.map((call) => (
               <div key={call.id}>
-                <div className="border-b bg-white shadow rounded-lg mt-5">
-                  <div className="flex items-center gap-3 py-4 px-2.5">
-                    <span className="font-bold text-2xl">{call.icon}</span>
-                    <div className="">
-                      <div className="flex items-center gap-2">
-                        <h1 className="font-bold text-lg">{call.type}</h1>
-                        <p className="text-gray-500">
-                          <span>with</span> {call.expecteFriend.name}
-                        </p>
-                      </div>
-
-                      <h1 className="text-gray-500">
-                        {new Date(call.timestamp).toLocaleDateString("en-US", {
-                          year: "numeric",
-                          month: "long",
-                          day: "numeric",
-                        })}
-                      </h1>
-                    </div>
-                  </div>
-                </div>
+                <TimeLineCard call={call} />
               </div>
             ))}
           </div>
